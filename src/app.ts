9dcdef301ed04d041631d53481d257b2f9622c64
Alin Salaru <alin.salaru@systematic.com>
2017-01-03 12:12:54 +0200

    add decorators

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-class Person {
-    name = "Sally"
+function addAge(anAge : Number) {
+    return function(targetClass) {
+        return class {
+            name= new targetClass().name;
+            age= anAge;
+        }
+    }
 }
+
+@addAge(20)
+class Person {
+    name = "Bob"
+}
+
+console.log(new Person());
